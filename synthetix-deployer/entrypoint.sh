212cda3ecb11c3eba71ed12fbaa416f30e9577ed
Mark Tyneway <mark.tyneway@gmail.com>
2021-01-07 13:49:11 -0800

    syntetix-docker: fixes

--- synthetix-deployer/entrypoint.sh
+++ synthetix-deployer/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# For test deployments only
+
+# Copyright Optimism PBC 2021
+# MIT License
+# github.com/ethereum-optimism
+
 DIR=$1
 if [ -z $DIR ]; then
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
@@ -7,13 +13,13 @@ fi
 
 cd $DIR
 
-DEPLOYMENT_PATH="publish/deployed/test-ovm"
-
-# The config options are currently hard coded
-# to only work with `optimism-integration` setup
-node $DIR/publish deploy-ovm-pair
+node $DIR/publish deploy-ovm-pair \
+    --l1-provider-url $L1_NODE_WEB3_URL \
+    --l2-provider-url $L2_NODE_WEB3_URL \
+    --data-provider-url $DEPLOYER_HTTP
 
 echo "Starting HTTP server on $SERVER_PORT"
+DEPLOYMENT_PATH="$DIR/publish/deployed/local"
 python \
     -m http.server \
     --bind 0.0.0.0 $SERVER_PORT \
