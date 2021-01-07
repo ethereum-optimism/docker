3d3b19ba89518101168d4c92391d3e36dc65c1b3
Mark Tyneway <mark.tyneway@gmail.com>
2021-01-07 11:56:23 -0800

    synthetix-deployer: modernize

--- synthetix-deployer/wait-for-node.sh
+++ synthetix-deployer/wait-for-node.sh
@@ -6,46 +6,35 @@
 
 cmd="$@"
 JSON='{"jsonrpc":"2.0","id":0,"method":"net_version","params":[]}'
-SYNTHETIX_TARGET=${SYNTHETIX_TARGET:-'L2'}
-
-# This script is generalized to work with either L1 or L2.
-NODE_WEB3_URL=$NODE_WEB3_URL
-if [[ "$SYNTHETIX_TARGET" == "L2" ]]; then
-    NODE_WEB3_URL=$L2_NODE_WEB3_URL
-elif [[ "$SYNTHETIX_TARGET" == "L1" ]]; then
-    NODE_WEB3_URL=$L1_NODE_WEB3_URL
-fi
 
 RETRIES=20
 until $(curl --silent --fail \
     --output /dev/null \
     -H "Content-Type: application/json" \
-    --data "$JSON" "$NODE_WEB3_URL"); do
+    --data "$JSON" "$L1_NODE_WEB3_URL"); do
   sleep 1
-  echo "Will wait $((RETRIES--)) more times for $NODE_WEB3_URL to be up..."
+  echo "Will wait $((RETRIES--)) more times for $L1_NODE_WEB3_URL to be up..."
 
   if [ "$RETRIES" -lt 0 ]; then
-    echo "Timeout waiting for remote node at $NODE_WEB3_URL"
+    echo "Timeout waiting for remote node at $L1_NODE_WEB3_URL"
     exit 1
   fi
 done
+echo "Connected to L1 Node at $L1_NODE_WEB3_URL"
 
-echo "Connected to Remote Node at $NODE_WEB3_URL"
+RETRIES=30
+until $(curl --silent --fail \
+    --output /dev/null \
+    -H "Content-Type: application/json" \
+    --data "$JSON" "$L2_NODE_WEB3_URL"); do
+  sleep 1
+  echo "Will wait $((RETRIES--)) more times for $L2_NODE_WEB3_URL to be up..."
 
-# Register the contracts with hardhat if the backend is hardhat
-if [[ ! -z "$HARDHAT" && "$SYNTHETIX_TARGET" == "L1" ]]; then
-    BUILD_DIR=/opt/synthetix/build
-    METHOD="hardhat_addCompilationResult"
-    VERSION=$(cat $BUILD_DIR/cache/last-solc-config.json | jq -r .solc.version)
-    INPUT=$(cat $BUILD_DIR/cache/solc-input.json)
-    OUTPUT=$(cat $BUILD_DIR/cache/solc-output.json)
-    DATA='{"jsonrpc":"2.0","method":"'$METHOD'","params":["'$VERSION'",'"$INPUT"','"$OUTPUT"'],"id":0}'
-    echo "$DATA" | curl --silent --output /dev/null -X POST \
-        -H 'Content-Type: application/json' \
-        --data @- \
-        $NODE_WEB3_URL
-fi
+  if [ "$RETRIES" -lt 0 ]; then
+    echo "Timeout waiting for layer two node at $L2_NODE_WEB3_URL"
+    exit 1
+  fi
+done
+echo "Connected to L2 Node at $L2_NODE_WEB3_URL"
 
-exec env \
-    NODE_WEB3_URL=$NODE_WEB3_URL \
-    $cmd
+exec $cmd
