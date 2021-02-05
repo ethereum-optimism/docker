572032556a8b8f7d27de906ebc377d413501c9b5
Kelvin Fichter <kelvinfichter@gmail.com>
2021-02-05 18:41:30 -0500

    Add a dockerfile for the DTL

---
+++ data-transport-layer/wait-for-l1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Copyright Optimism PBC 2020
+# MIT License
+# github.com/ethereum-optimism
+
+cmd="$@"
+JSON='{"jsonrpc":"2.0","id":0,"method":"net_version","params":[]}'
+
+RETRIES=20
+until $(curl --silent --fail \
+    --output /dev/null \
+    -H "Content-Type: application/json" \
+    --data "$JSON" "$L1_NODE_WEB3_URL"); do
+  sleep 1
+  echo "Will wait $((RETRIES--)) more times for $L1_NODE_WEB3_URL to be up..."
+
+  if [ "$RETRIES" -lt 0 ]; then
+    echo "Timeout waiting for layer one node at $L1_NODE_WEB3_URL"
+    exit 1
+  fi
+done
+
+echo "Connected to L1 Node at $L1_NODE_WEB3_URL"
